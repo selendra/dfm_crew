d438c30c5c553598d3a92c29f04219538f34143d
DaveatCor <condaveat@gmail.com>
2022-12-21 10:05:02 +0800

    fix sound loop, 21/Dec/22

--- lib/tool/sound.dart
+++ lib/tool/sound.dart
@@ -14,7 +14,7 @@ class SoundUtil {
     await FlutterRingtonePlayer.play(
       fromAsset: 'assets/sounds/$soundName',
       ios: IosSounds.glass,
-      looping: true, // Android only - API >= 28
+      looping: false, // Android only - API >= 28
       volume: 1.0, // Android only - API >= 28
       asAlarm: false, // Android only - all APIs
     );
